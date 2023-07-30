de79afade2b472dc2565615b3d748d98fc85f240
adu224256 <99267460+adu224256@users.noreply.github.com>
2023-07-30 12:38:40 +0800

    0.2.1 update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="interactions-argtask",
-    version="0.2.0",
+    version="0.2.1",
     description="",
     long_description=long_description,
     long_description_content_type="text/markdown",
